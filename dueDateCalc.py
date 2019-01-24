ca274b199165f209501ee9fc058c844738636b67
Jim Strieter <jimstrieter@gmail.com>
2019-01-24 15:28:37 -0700

    Added some tweaks

--- dueDateCalc.py
+++ dueDateCalc.py
@@ -2,16 +2,19 @@
 
 from datetime import datetime, timedelta
 
-KNOWN_PAYDAY = "2018-01-01"
-PERIODS_FWD = 5
+def twoDecimals(x):
+    return int(x*100)/100
 
 class book:
-    def __init__(self):
+    def __init__(self, knownPayday):
         x = {}
         for m in range(-3, 32):
             x[m] = []
         self.book = x
-        self.dt = datetime.strptime(KNOWN_PAYDAY, "%Y-%M-%d")
+        self.dt = datetime.strptime(knownPayday, "%Y-%M-%d")
+        self.payCheck=1 # Set this manually before using
+        self.savings=0
+        self.saveRatio=0.5
 
     def addExpense(self, nm, dayNum, amt):
         self.book[dayNum].append({"name":nm, "amount":amt})
@@ -19,13 +22,17 @@ class book:
     def oneCycle(self):
         dStart = self.dt
         tot = 0
-        rep = ''
+        rep = '' # Report string
         for m in range(0, 14):
             k = self.dt.day
             for n in self.book[k]:
                 tot += n["amount"]
                 rep += str(k) + " " + n["name"] + " : " + str(n["amount"]) + "\n"
             self.dt = self.dt + timedelta(1)
+        deltaSavings = self.saveRatio * (self.payCheck-tot)
+        self.savings += deltaSavings
+        rep += "Money Saved This Pay Cycle: " + str(twoDecimals(deltaSavings)) + "\n"
+        rep += "Total Saved: " + str(twoDecimals(self.savings)) + "\n"
         return [tot, rep]
 
     def nCycles(self, N):
@@ -33,11 +40,11 @@ class book:
             print("For pay period starting: ", str(self.dt))
             x, y = self.oneCycle()
             print(y)
-            print("Total is ", x)
+            print("Expenses This Pay Cycle:   ", twoDecimals(x))
             print("\n\n")
 
 if __name__=="__main__":
-    b = book()
+    b = book("2018-01-01")
     b.addExpense("house", 1, 1000)
     b.addExpense("car", 2, 380)
     b.addExpense("credit 1", 13, 300)
